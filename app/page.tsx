d036bf4410502e3bf71bf5433bbaf6d05d555193
Letam <davidsonletam@gmail.com>
2025-09-25 11:58:06 +0100

    work in progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,65 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	return (
+		<div>
+			<div className="flex justify-center items-center w-full border-b border-[#D8D8D8] py-6">
+				<div className="relative w-[130px] h-[30px] xs:w-[180px] xs:h-[38px]">
+					<Image
+						className="object-cover"
+						src="/logo.svg"
+						alt="Print padi logo"
+						fill
+						priority
+					/>
+				</div>
+			</div>
+			<div className="flex px-3 flex-col justify-center items-center xs:flex-row xs:items-center h-[80vh] xs:justify-center xs:space-x-16">
+				<div className="w-full xs:max-w-[553px] flex flex-col justify-center items-center xs:items-start">
+					<h3 className="font-bold text-5xl xs:text-[56px] text-center xs:text-left">
+						⚠️Page Under Construction
+					</h3>
+					<div className="flex xs:hidden">
+						<Image
+							className=""
+							src="/caution.png"
+							alt="site under construction"
+							height={300}
+							width={300}
+							priority
+						/>
+					</div>
+					<p className="font-normal text-xl text-center xs:text-left xs:text-2xl mt-3 mb-8">
+						We’re sorry for the inconvenience. While we work on improving your
+						experience, you can still reach us directly on WhatsApp.
+					</p>
+					<Link href={"https://wa.me/message/6J4E5NXS4LHSL1"} className="">
+						<button
+							aria-label="continue on whatsapp"
+							className="flex items-center space-x-3 cursor-pointer rounded-lg bg-black text-white font-medium text-lg xs:text-2xl py-2.5 xs:py-[18px] px-3"
+						>
+							<div className="h-5 w-5 xs:h-[33px] xs:w-[33px] relative">
+								<Image
+									className="object-cover"
+									src="/whatsapp-icon.svg"
+									alt="continue on whatsapp"
+									fill
+								/>
+							</div>
+							<span>Order on WhatsApp</span>
+						</button>
+					</Link>
+				</div>
+				<Image
+					className="hidden xs:flex"
+					src="/caution.png"
+					alt="site under construction"
+					height={500}
+					width={500}
+					priority
+				/>
+			</div>
+		</div>
+	);
 }
